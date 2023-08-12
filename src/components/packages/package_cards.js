d5157427ae8705929c517b15b516d7a43224dc1d
lit-poks <pokhrel.lalit99@gmail.com>
2023-08-12 21:38:44 +0600

    cards ready for packages

--- src/components/packages/package_cards.js
+++ src/components/packages/package_cards.js
@@ -2,26 +2,35 @@
 function PackageCards() {
     return (
         <>
-            <div className="flex flex-grow justify-center text-white">
+            <div className="flex flex-grow justify-center text-white font-bold text-2xl p-2">
                 <div id="cultural-tours"
-                 className="flex items-end justify-center flex-grow w-40 max-w-2xl max-h-[rem] m-4 rounded-lg shadow">
-                    <h1 className="text-bold text-xl p-2">
+                    className="group flex items-end justify-center flex-grow w-40 max-w-xl max-h-[rem] m-4 rounded-lg shadow hover:scale-90 hover:duration-150">
+                    <h1 className="group-hover:-translate-y-12 group-hover:duration-300">
                         Cultural Tours
                     </h1>
                 </div>
-                <div id="trekking-tours" 
-                className="flex items-end justify-center flex-grow w-40 max-w-2xl max-h-[rem] m-4 rounded-lg shadow">
-                    Trekking and Fishing Tours
+                <div id="trekking-tours"
+                    className="group flex items-end justify-center flex-grow w-40 max-w-xl max-h-[rem] m-4 rounded-lg shadow hover:scale-90 hover:duration-150">
+                    <h1 className="group-hover:-translate-y-12 group-hover:duration-300">
+                        Trekking & Fishing Tours
+                    </h1>
+
                 </div>
             </div>
-            <div className="justify-self-start flex flex-grow justify-center text-white">
+            <div className="flex flex-grow justify-center text-white font-bold text-2xl p-2">
                 <div id="honeymoon-tours"
-                 className="flex items-end justify-center flex-grow w-40 max-w-2xl max-h-[rem] m-4 rounded-lg shadow">
-                    Honeymoons and Wedding
+                    className="group flex items-end justify-center flex-grow w-40 max-w-xl max-h-[rem] m-4 rounded-lg shadow hover:scale-90 hover:duration-150">
+                    <h1 className="group-hover:-translate-y-12 group-hover:duration-300">
+                        Honeymoons and Wedding
+                    </h1>
+
                 </div>
                 <div id="special-packages"
-                 className="flex items-end justify-center flex-grow w-40 max-w-2xl max-h-[rem] m-4 rounded-lg shadow">
-                    Special packages
+                    className="group flex items-end justify-center flex-grow w-40 max-w-xl max-h-[rem] m-4 rounded-lg shadow hover:scale-90 hover:duration-150">
+                    <h1 className="group-hover:-translate-y-12 group-hover:duration-300">
+                        Special packages
+                    </h1>
+
                 </div>
             </div>
         </>
